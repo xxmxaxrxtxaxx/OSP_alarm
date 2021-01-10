cbc2a1a84f60ea6db75954fd11a72e30d9765441
Marta <35894571+xxmxaxrxtxaxx@users.noreply.github.com>
2021-01-10 15:34:24 +0100

    poprawiona mapa

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,9 @@
 (function(){
   function init(){
     var centerMap=new google.maps.LatLng(53.45275691550154, 17.124603872327434);
+    var ddd=new google.maps.LatLng(53.45043554294898, 17.092494321306425);
+
+ 
       var mapOptions={
         center: centerMap,
         zoom: 11,
@@ -16,6 +19,13 @@
 
       });
       marker.setMap(map);
+      var m = new google.maps.Marker({
+        position: ddd,
+        //icon: "/pin.png",
+        label: "ddff"
+
+      });
+      m.setMap(map);
   }
   google.maps.event.addDomListener(window, 'load', init);
 })();
